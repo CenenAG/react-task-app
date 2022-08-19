c244c4afbe966a656fb713570475394077a5fbd7
CenenAG <agtecnologia.mex@gmail.com>
2022-08-19 15:54:47 -0700

    Commit inicial de Training React

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { tasks as data } from "./data/tasks";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle, taskDescription) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length,
+        description: taskDescription,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  }
+
+  return (
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </>
+  );
+}
+
+export default App;
